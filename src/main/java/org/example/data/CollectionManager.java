3e8d361485363e7bc59d4680469b3d1f98373c59
Bot <slava54452005@gmail.com>
2026-03-13 16:55:59 +0300

    Updated code

---
+++ src/main/java/org/example/data/CollectionManager.java
@@ -0,0 +1,12 @@
+package org.example.data;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CollectionManager {
+    private Map<Integer, Person> collection = new TreeMap<>();//String? может T<>
+
+
+
+
+}
